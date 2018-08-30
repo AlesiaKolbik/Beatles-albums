15ba99e70d0ff6456bec0d8cd0e9bf2554cb38b2
AlesiaKolbik <32718331+AlesiaKolbik@users.noreply.github.com>
2018-08-30 12:25:38 +0300

    Initial commit

---
+++ templates.js
@@ -0,0 +1,6 @@
+<img src='${img}' alt='${name}' title='${name}'>
+    <div class='SName'>Название: <span>${name}</span></div>
+<div class='SYear'>Год выхода: <span>${year}</span></div>
+{{if place}}
+<div class='SPlace'>RollingStone: <span>${place}</span></div>
+{{/if}}
